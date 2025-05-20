9dc48214807330938e2b235678bda283e1ae6df1
Crystal <crystalray@qq.com>
2025-05-20 16:01:54 +0800

    迁移

--- config/database.php
+++ config/database.php
@@ -23,5 +23,26 @@ return [
                 'heartbeat_interval' => 35,
             ],
         ],
+        'org' => [
+            'driver' => 'pgsql',
+            'host' => yaml('pgsql.org.host'),
+            'port' => yaml('pgsql.org.port'),
+            'database' => yaml('pgsql.org.database'),
+            'username' => yaml('pgsql.org.username'),
+            'password' => yaml('pgsql.org.password'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+            'options' => [
+            ],
+            'pool' => [
+                'max_connections' => 5,
+                'min_connections' => 0,
+                'wait_timeout' => 3,
+                'idle_timeout' => 60,
+                'heartbeat_interval' => 35,
+            ],
+        ],
     ],
 ];
---
+++ scripts/Migration.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace scripts;
+
+/**
+ * 执行从v1数据库迁移数据的任务
+ */
+class Migration
+{
+
+}
