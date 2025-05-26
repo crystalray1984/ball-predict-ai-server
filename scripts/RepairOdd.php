5622aecefecc694e11a8044ebbdf80c4ea5088f2
Crystal <crystalray@qq.com>
2025-05-26 11:03:26 +0800

    迁移脚本

--- scripts/RepairOdd.php
+++ scripts/RepairOdd.php
@@ -1,46 +1,92 @@
 <?php declare(strict_types=1);
 
+namespace scripts;
+
+use app\model\Match1;
+use app\model\Odd;
+use app\model\PromotedOdd;
+
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../support/bootstrap.php";
 
-$service = G(\app\admin\service\MatchService::class);
+class RepairOdd
+{
+    public function run(): void
+    {
+        //为标记为skip的盘口生成对应的推荐
+        $this->processSkipOdds();
+    }
 
-$odds = \app\model\PromotedOdd::query()
-    ->whereIn('type', ['over', 'under'])
-    ->whereNotNull('result')
-    ->where('created_at', '>=', '2025/05/20 0:00:00+08:00')
-    ->get(['match_id', 'period'])
-    ->toArray();
+    /**
+     * 为标记为skip的盘口生成对应的推荐
+     * @return void
+     */
+    protected function processSkipOdds(): void
+    {
+        $matches = [];
 
-foreach ($odds as $odd) {
+        $odds = Odd::query()
+            ->where('status', '=', 'skip')
+            ->get()
+            ->toArray();
 
-    $match = \app\model\Match1::query()
-        ->where('id', '=', $odd['match_id'])
-        ->first([
-            'score1',
-            'score2',
-            'corner1',
-            'corner2',
-            'score1_period1',
-            'score2_period1',
-            'corner1_period1',
-            'corner2_period1'
-        ]);
-    if(!$match) continue;
-    $match = $match->toArray();
+        foreach ($odds as $odd) {
+            if (empty($matches[$odd['match_id']])) {
+                $match = Match1::query()
+                    ->where('id', '=', $odd['match_id'])
+                    ->first()
+                    ->toArray();
+                if (!$match['has_score']) {
+                    Odd::query()
+                        ->where('id', '=', $odd['id'])
+                        ->update([
+                            'status' => 'promoted',
+                        ]);
+                    continue;
+                }
+                $matches[$odd['match_id']] = $match;
+            } else {
+                $match = $matches[$odd['match_id']];
+            }
 
-    echo '重新计算赛果 ' . $odd['match_id'] . PHP_EOL;
+            $promoted = [
+                'match_id' => $odd['match_id'],
+                'odd_id' => $odd['id'],
+                'skip' => 'setting',
+                'variety' => $odd['variety'],
+                'period' => $odd['period'],
+                'type' => match ($odd['type']) {
+                    'ah1' => 'ah2',
+                    'ah2' => 'ah1',
+                    'over' => 'under',
+                    'under' => 'over',
+                },
+                'condition' => match ($odd['type']) {
+                    'ah1', 'ah2' => bcsub('0', $odd['condition']),
+                    default => $odd['condition'],
+                },
+                'back' => 1,
+            ];
 
-    $service->setMatchScore([
-        'match_id' => $odd['match_id'],
-        'score1' => $match['score1'],
-        'score2' => $match['score2'],
-        'corner1' => $match['corner1'],
-        'corner2' => $match['corner2'],
-        'score1_period1' => $match['score1_period1'],
-        'score2_period1' => $match['score2_period1'],
-        'corner1_period1' => $match['corner1_period1'],
-        'corner2_period1' => $match['corner2_period1'],
-        'period1' => $odd['period'] === 'period1'
-    ]);
+            $result = get_odd_score($match, $promoted);
+            $promoted += [
+                'result' => $result['result'],
+                'result1' => $result['result'],
+                'score' => $result['score'],
+                'score1' => $result['score1'],
+                'score2' => $result['score2'],
+            ];
+
+            PromotedOdd::insert($promoted);
+            Odd::query()
+                ->where('id', '=', $odd['id'])
+                ->update([
+                    'status' => 'promoted',
+                ]);
+
+            echo "添加推荐 odd_id=" . $odd['id'] . PHP_EOL;
+        }
+    }
 }
+
+(new RepairOdd())->run();
