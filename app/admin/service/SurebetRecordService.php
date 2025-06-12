06f2543cb71bd55b34d62f8ac2f6376409bd596b
Crystal <crystalray@qq.com>
2025-06-12 18:45:40 +0800

    surebet推送记录导入

--- app/admin/service/SurebetRecordService.php
+++ app/admin/service/SurebetRecordService.php
@@ -71,86 +71,89 @@ class SurebetRecordService
                 'v_match.corner1_period1',
                 'v_match.corner2_period1',
             ])
-            ->chunk(500, function (object $row) use (&$rows) {
-                $row = (array)$row;
-
-                //看看有没有对应的赛果(只针对支持的投注类型)
-                if (
-                    $row['game'] === 'regular'
-                    && $row['base'] === 'overall'
-                    && in_array($row['variety'], ['corner', 'goal'])
-                    && in_array($row['period'], ['regularTime', 'period1'])
-                    && in_array($row['type'], ['ah1', 'ah2', 'over', 'under'])
-                ) {
+            ->chunk(500, function ($_list) use (&$rows) {
+                $list = $_list->toArray();
+                foreach ($list as $row) {
+                    //看看有没有对应的赛果(只针对支持的投注类型)
+                    $virtual_odd = [];
+                    
                     if (
-                        $row['period'] === 'period1' && $row['has_period1_score'] ||
-                        $row['period'] === 'regularTime' && $row['has_score']
+                        $row['game'] === 'regular'
+                        && $row['base'] === 'overall'
+                        && in_array($row['variety'], ['corner', 'goal'])
+                        && in_array($row['period'], ['regularTime', 'period1'])
+                        && in_array($row['type'], ['ah1', 'ah2', 'over', 'under'])
                     ) {
-                        //虚拟盘口数据
-                        [$type, $condition] = get_reverse_odd($row['type'], $row['condition']);
+                        if (
+                            $row['period'] === 'period1' && $row['has_period1_score'] ||
+                            $row['period'] === 'regularTime' && $row['has_score']
+                        ) {
+                            //虚拟盘口数据
+                            [$type, $condition] = get_reverse_odd($row['type'], $row['condition']);
 
-                        $virtual_odd = [
-                            'variety' => $row['variety'],
-                            'period' => $row['period'],
-                            'type' => $type,
-                            'condition' => $condition,
-                        ];
+                            $virtual_odd = [
+                                'variety' => $row['variety'],
+                                'period' => $row['period'],
+                                'type' => $type,
+                                'condition' => $condition,
+                            ];
 
-                        $result = get_odd_score($row, $virtual_odd);
-                        $virtual_odd['score'] = $result['score'];
-                        $virtual_odd['result'] = $result['result'];
+                            $result = get_odd_score($row, $virtual_odd);
+                            $virtual_odd['score'] = $result['score'];
+                            $virtual_odd['result'] = $result['result'];
+                        }
                     }
+
+
+                    //模拟盘口数据
+                    $virtual_text = '';
+                    $virtual_result = '';
+                    $virtual_score = '';
+
+                    if (!empty($virtual_odd)) {
+                        $virtual_type = match ($virtual_odd['type']) {
+                            'ah1' => '主胜',
+                            'ah2' => '客胜',
+                            'over' => '大球',
+                            'under' => '小球',
+                            default => '',
+                        };
+                        $virtual_condition = match ($virtual_odd['type']) {
+                            'ah1', 'ah2' => (bccomp($virtual_odd['condition'], '0', 2) > 0 ? '+' : '') . (float)$virtual_odd['condition'],
+                            default => (string)(float)$virtual_odd['condition'],
+                        };
+
+                        $virtual_text = "$virtual_type $virtual_condition";
+                        $virtual_result = match ($virtual_odd['result']) {
+                            0 => '和',
+                            1 => '赢',
+                            -1 => '输',
+                            default => '',
+                        };
+                        $virtual_score = $virtual_odd['score'];
+                    }
+
+                    //写入数据
+                    $rows[] = [
+                        $row['id'],
+                        $row['match_id'],
+                        $row['tournament_name'],
+                        Carbon::parse($row['match_time'])->toDateTimeString(),
+                        $row['team1_name'],
+                        $row['team2_name'],
+                        $row['game'],
+                        $row['base'],
+                        $row['period'],
+                        $row['variety'],
+                        $row['type'],
+                        $row['condition'],
+                        $row['value'],
+                        Carbon::parse($row['created_at'])->toDateTimeString(),
+                        $virtual_text,
+                        $virtual_result,
+                        $virtual_score,
+                    ];
                 }
-
-
-                //模拟盘口数据
-                $virtual_text = '';
-                $virtual_result = '';
-                $virtual_score = '';
-
-                if (!empty($virtual_odd)) {
-                    $virtual_type = match ($virtual_odd['type']) {
-                        'ah1' => '主胜',
-                        'ah2' => '客胜',
-                        'over' => '大球',
-                        'under' => '小球',
-                        default => '',
-                    };
-                    $virtual_condition = match ($virtual_odd['type']) {
-                        'ah1', 'ah2' => (bccomp($virtual_odd['condition'], '0', 2) > 0 ? '+' : '') . (float)$virtual_odd['condition'],
-                        default => (string)(float)$virtual_odd['condition'],
-                    };
-
-                    $virtual_text = "$virtual_type $virtual_condition";
-                    $virtual_result = match ($virtual_odd['result']) {
-                        0 => '和',
-                        1 => '赢',
-                        -1 => '输',
-                        default => '',
-                    };
-                    $virtual_score = $virtual_odd['score'];
-                }
-
-                //写入数据
-                $rows[] = [
-                    $row['id'],
-                    $row['match_id'],
-                    $row['tournament_name'],
-                    Carbon::parse($row['match_time'])->toDateTimeString(),
-                    $row['team1_name'],
-                    $row['team2_name'],
-                    $row['game'],
-                    $row['base'],
-                    $row['period'],
-                    $row['variety'],
-                    $row['type'],
-                    $row['condition'],
-                    $row['value'],
-                    Carbon::parse($row['created_at'])->toDateTimeString(),
-                    $virtual_text,
-                    $virtual_result,
-                    $virtual_score,
-                ];
             });
 
         $spreadsheet = new Spreadsheet();
