c1c93e5cf8001a60e4163a8b0058a1a121396d88
Crystal <crystalray@qq.com>
2026-03-18 14:12:35 +0800

    模型3放入前端

--- config/channel.php
+++ config/channel.php
@@ -27,5 +27,10 @@ return [
     [
         'key' => 'mansion',
         'name' => '模型2',
-    ]
+    ],
+    [
+        'key' => 'model3',
+        'name' => '模型3',
+        'use_profit' => true,
+    ],
 ];
