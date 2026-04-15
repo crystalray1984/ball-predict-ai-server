01eb4913512eca30c2e7f268d90d9d4d3b673e0e
Crystal <crystalray@qq.com>
2026-04-15 22:50:47 +0800

    修改频道设置

--- config/channel.php
+++ config/channel.php
@@ -4,43 +4,55 @@
  * 频道设定
  */
 return [
+//    [
+//        'key' => 'rockball',
+//        'name' => '滚球',
+//        'report' => true,
+//        'filter' => 'rockball_filter',
+//    ],
+//    [
+//        'key' => 'rockball2',
+//        'name' => '滚球2',
+//        'report' => true,
+//    ],
+//    [
+//        'key' => 'rockball3',
+//        'name' => '滚球3',
+//        'use_profit' => true,
+//    ],
+//    [
+//        'key' => 'rockball4',
+//        'name' => '滚球4',
+//        'use_profit' => true,
+//    ],
     [
-        'key' => 'rockball',
-        'name' => '滚球',
-        'report' => true,
-        'filter' => 'rockball_filter',
+        'key' => 'ai_ah',
+        'name' => '让球',
     ],
     [
-        'key' => 'rockball2',
-        'name' => '滚球2',
-        'report' => true,
+        'key' => 'ai_sum',
+        'name' => '大小球',
     ],
     [
-        'key' => 'rockball3',
-        'name' => '滚球3',
-        'use_profit' => true,
-    ],
-    [
-        'key' => 'rockball4',
-        'name' => '滚球4',
-        'use_profit' => true,
+        'key' => 'ai_win',
+        'name' => '胜平负',
     ],
     [
         'key' => 'rockball5',
-        'name' => '滚球5',
+        'name' => '滚球',
 //        'use_profit' => true,
     ],
 //    [
 //        'key' => 'direct',
 //        'name' => '模型1',
 //    ],
-    [
-        'key' => 'mansion',
-        'name' => '模型2',
-    ],
-    [
-        'key' => 'model3',
-        'name' => '模型3',
-        'use_profit' => true,
-    ],
+//    [
+//        'key' => 'mansion',
+//        'name' => '模型2',
+//    ],
+//    [
+//        'key' => 'model3',
+//        'name' => '模型3',
+//        'use_profit' => true,
+//    ],
 ];
